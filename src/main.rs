2a331a6f6a498f51b83828a91153744a45c4e825
Uggla <uggla@free.fr>
2026-01-08 19:20:25 +0100

    fix: Log name

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::env;
 
-use async_rust_tui::{api_check, run};
+use async_rust_tui::{APPNAME, api_check, run};
 use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 fn main() -> anyhow::Result<()> {
     // Setup logging
-    let file_appender = tracing_appender::rolling::daily("logs", "ratatai.log");
+    let file_appender = tracing_appender::rolling::daily("logs", format!("{}.log", APPNAME));
     let (non_blocking_appender, _guard) = tracing_appender::non_blocking(file_appender);
 
     let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
